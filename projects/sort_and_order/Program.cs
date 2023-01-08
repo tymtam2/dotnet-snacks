7decafc9013e6ccd0fbd6afd2ed88ec748c0fadc
Tymek Majewski <tmaj@pm.me>
2023-01-08 14:46:53 +1000

    Fixed comparability for record types

--- projects/sort_and_order/Program.cs
+++ projects/sort_and_order/Program.cs
@@ -3,10 +3,10 @@ using BenchmarkDotNet.Running;
 
 var summary = BenchmarkRunner.Run(
     types: new [] {
-        typeof(SortAndOrderBenchmarkInt), 
-        typeof(SortAndOrderBenchmarkString),
+      //  typeof(SortAndOrderBenchmarkInt), 
+      //  typeof(SortAndOrderBenchmarkString),
         typeof(SortAndOrderBenchmarkRecordStruct),
-        typeof(SortAndOrderBenchmarkRecordClass)
+      //  typeof(SortAndOrderBenchmarkRecordClass)
         });
 
 
@@ -20,15 +20,20 @@ public class SortAndOrderBenchmarkString: SortAndOrderBenchmarks<string>
     public SortAndOrderBenchmarkString() : base((Random r) => r.Next().ToString()) {}
 }
 
-public record RClassInt(int I);
+public record RClassInt(int I): IComparable<RClassInt>
+{
+    public int CompareTo(RClassInt? other) => I.CompareTo(other?.I);
+}
 
 public class SortAndOrderBenchmarkRecordClass: SortAndOrderBenchmarks<RClassInt>
 {
     public SortAndOrderBenchmarkRecordClass() : base((Random r) => new RClassInt(r.Next())) {}
 }
 
-public record struct RStructInt(int I);
-
+public record struct RStructInt(int I) : IComparable<RStructInt>
+{
+    public int CompareTo(RStructInt other) => I.CompareTo(other.I);
+}
 public class SortAndOrderBenchmarkRecordStruct: SortAndOrderBenchmarks<RStructInt>
 {
     public SortAndOrderBenchmarkRecordStruct() : base((Random r) => new RStructInt(r.Next())) {}
